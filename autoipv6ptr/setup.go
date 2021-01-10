ed4e1c3f2b36582700f6269f20d0f7f3d0864145
Josef Stautner <hello@veloc1ty.de>
2021-01-10 15:18:05 +0100

    Update to be coredns 1.8.1 compatible

--- autoipv6ptr/setup.go
+++ autoipv6ptr/setup.go
@@ -10,16 +10,13 @@ import (
 
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
-	"github.com/caddyserver/caddy"
+	"github.com/coredns/caddy"
 )
 
 const AUTOIPV6PTR_PLUGIN_NAME string = "autoipv6ptr"
 
 func init() {
-	caddy.RegisterPlugin(AUTOIPV6PTR_PLUGIN_NAME, caddy.Plugin{
-		ServerType: "dns",
-		Action:     setup,
-	})
+	plugin.Register(AUTOIPV6PTR_PLUGIN_NAME, setup)
 }
 
 func setup(c *caddy.Controller) error {
